60aaf12ca1c48749888f93c6728af403e5d35b67
Ana Selvaraj <ana@selvaraj.org>
2020-03-28 16:30:43 -0700

    Upload button working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import ImageUploader from 'react-images-upload';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>95Defender</h1>
+      <ImageConversion/>
     </div>
   );
 }
+
+class ImageConversion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { image: null, masked_image: null };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(picture) {
+    this.setState({ image: picture });
+    //this.handleSubmit();
+  }
+
+  handleSubmit = () => {
+    const formData = new FormData();
+    formData.append(
+      'image', this.state.image
+    )
+    formData.append('responseType', 'stream');
+    let url = '';
+    axios.get(url, formData)
+      .then(response => {
+        this.setState({ masked_image: response.data });
+      })
+  }
+
+  render() {
+    if (this.state.masked_image) {
+      return (
+        <div className="imageConversion">
+          <ImageUploader withIcon={true} buttonText="Upload new image"
+            onChange={this.onDrop}
+            imgExtension={[".jpg", ".png"]}
+            maxFileSize={5242880} 
+            singleImage={true}
+            label ='Max File Size: 5MB, accepted: .JPG | .PNG'
+            />
+          <img src={this.state.masked_image} alt="Protected image"></img>
+        </div>
+      );
+    } else {
+      return (
+        <div className="imageConversion">
+          <ImageUploader withIcon={true} buttonText="Upload image"
+            onChange={this.onDrop}
+            imgExtension={[".jpg", ".png"]}
+            singleImage={true}
+            label ='Max File Size: 5MB, accepted: .JPG | .PNG'
+            maxFileSize={5242880} />
+        </div>
+      );
+    }
+  }
+}
+
+
 
 export default App;
